a98600073f9fcdd8ba7466270f144180abafb0e8
turingczz <920886811@163.com>
2025-10-31 12:52:51 +0800

    feat: test

--- script/DeployLogic.sol
+++ script/DeployLogic.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.8.22;
 
 import { Script } from "lib/forge-std/src/Script.sol";
 import { console } from "forge-std/console.sol";
-import {Ping} from "../src/Ping-2.sol";
+import {Ping} from "../src/v4.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
 
 contract DeployScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
-
         Ping imp = new Ping();
-
         vm.stopBroadcast();
 
         console.log("logic deployed to:", address(imp));
